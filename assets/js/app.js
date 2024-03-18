3f36124797fca19983ecbbfa00042bd5d02d9325
Natouille <ngouveia@edenschool.fr>
2024-03-18 13:10:04 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
-const p = document.querySelector("p");
+const main = document.querySelector("main");
 
 async function displayPost() {
-    const reponse = await fetch("https://www.reddit.com/r/letsnotmeet/new.json?limit=1");
-    const post = await reponse.json();
-    console.log(post.data.children[0].data.selftext);
+    const response = await fetch("https://www.reddit.com/r/letsnotmeet/new.json?limit=10");
+    const post = await response.json();
 
-    p.textContent = post.data.children[0].data.selftext;
+    for (let i = 0; i < post.data.children.length; i++) {
+
+        console.log(post.data.children[i].data);
+
+        const article = document.createElement("article");
+        const name = document.createElement("h2");
+        const story = document.createElement("p");
+        const author = document.createElement("p");
+        const hr = document.createElement("hr");
+
+        main.appendChild(article);
+        article.appendChild(name);
+        article.appendChild(story);
+        article.appendChild(author);
+        article.appendChild(hr);
+
+        name.classList.add("name");
+        story.classList.add("story");
+        author.classList.add("author");
+
+        name.textContent = post.data.children[i].data.title;
+        story.textContent = post.data.children[i].data.selftext;
+        author.textContent = "Post by : " + post.data.children[i].data.author;
+
+    }
 }
 
-displayPost()
+displayPost();
